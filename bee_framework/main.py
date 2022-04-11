30cedf619934925b9639ca5cc6308e1415454c46
ekaterina_pchelenko <e.pchelenko@gmail.com>
2022-04-11 22:19:14 +0500

    lesson_2

--- bee_framework/main.py
+++ bee_framework/main.py
@@ -1,5 +1,7 @@
 import quopri
 
+from requests import PostMethod, GetMethod
+
 
 class Error404:
     def __call__(self, request):
@@ -17,14 +19,36 @@ class Framework:
         if not path.endswith('/'):
             path = f'{path}/'
 
+        request = {}
+        method = environ['REQUEST_METHOD']
+        request['method'] = method
+
+        if method == 'POST':
+            data = PostMethod().get_params(environ)
+            request['data'] = data
+            print(f'Post-запрос: {Framework.decode_value(data)}')
+        if method == 'GET':
+            data = GetMethod().get_params(environ)
+            request['request_params'] = data
+            print(f'Get-запрос: {data}')
+        print(request)
+
         if path in self.route_list:
             view = self.route_list[path]
         else:
             view = Error404()
-        request = {}
 
         for f in self.front_list:
             f(request)
         code, info = view(request)
         start_resp(code, [('Content-Type', 'text/html')])
         return [info.encode('utf-8')]
+
+    @staticmethod
+    def decode_value(data):
+        info = {}
+        for key, value in data.items():
+            val = bytes(value.replace('%', '=').replace("+", " "), 'UTF-8')
+            val_decode_str = quopri.decodestring(val).decode('UTF-8')
+            info[key] = val_decode_str
+        return info
